10bd4b9e7795670a1cb5515d2f8eef88e8aaf415
Esraa <73849360+esraa-asoo@users.noreply.github.com>
2022-11-23 01:35:48 +0200

    Update ssh_conf.sh

--- project/ssh_conf.sh
+++ project/ssh_conf.sh
@@ -1,1 +1,21 @@
+#!/usr/bin/env bash
 
+
+change_port(){
+	sed -i "s/#Port 22/Port $u/g" /etc/ssh/sshd_config
+	echo "ssh Port Changed to $u"
+}
+
+disable_root(){
+	echo -e "Modifying Deafult ssh Configuration...\ "
+	sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
+	echo -e "Remote Login using root credintioal will be denied...\  "
+}
+
+firewall_selin(){
+	echo "selinux configuring..."
+	semanage port -a -t ssh_port_t -p tcp $u
+	echo "Allow $u port through Firewall"
+	firewall-cmd --permanent --zone=public --add-port=$u/tcp
+	firewall-cmd --reload
+}
