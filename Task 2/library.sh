dda65069321491c941392e7d42441ed5bc23976f
Esraa <73849360+esraa-asoo@users.noreply.github.com>
2022-11-22 23:23:35 +0200

    Update library.sh

--- Task 2/library.sh
+++ Task 2/library.sh
@@ -1,1 +1,46 @@
+#!/usr/bin/env bash
 
+check_user_root_priv(){
+users = $(groups $USER)
+if grep -q 'wheel' <<< "$users" || [ $EUID -eq 0 ] || grep "$USER" "/etc/sudores"
+then
+    echo "User has root privilages."
+    else
+    echo "User does not have root privilages."
+    fi
+}
+
+change_ssh_pot(){
+  echo " Pleas, enter the new ssh port to change: "
+  read port
+  sed -i "s/#port 22/port $newport/g" /etc/ssh/sshd_config
+  echo " ssh port changed to $newport "
+}
+disable_root_log(){
+sed -i "s/permitRootLogin yes/permitRootLogin no/g" /etc/ssh/sshd_config
+systemctl reload sshd
+}
+
+add_user(){
+  echo"Enter the name of the user that you want to add: "
+  read user
+  if [ grep -q "$user" <<< $(grep "$user" /etc/passwd) ]
+  then
+      echo $(grep "$user" /etc/passwd)
+      else
+      echo "Do you want to add user to sudoers: [y/n]"
+      read anser
+   if ["$anser" == "y"] || [ $anser == "yes" ]
+    then
+     echo "$user ALL =(ALL) ALL " >> /etc/sudores
+     echo "$user is sudoer now..."
+  else
+     echo "$user is not sudoer..."
+     fi
+   fi
+}
+
+backup(){
+ echo "* * * * * tar -czf /etc/homedir.tar.gz $HOME " > "$HOME/backup.txt"
+   crontab "$HOME/backup.txt"
+}
