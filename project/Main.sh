6e5b21a4de5b4a7598d5d849fab685a59d2c2e71
Esraa <73849360+esraa-asoo@users.noreply.github.com>
2022-11-26 18:05:26 +0200

    Update Main.sh

--- project/Main.sh
+++ project/Main.sh
@@ -7,21 +7,21 @@ do
 	g=$(groups $USER)
 	if grep -q 'wheel' <<< "$g" || [ "$EUID" -eq 0 ] || [ "$(whoami)" == root ] || grep "$USER" "/etc/sudoers";
 	then
-    menu $u $a $s
+           menu $1 $2 $3
 		read n
 		case "$n" in
-			1) change_ssh_port "$u";;
+			1) change_ssh_port "$1";;
 			2) Disable_root_Login;;
-			3) firewall_selinux "$u";;
+			3) firewall_selinux "$1";;
 			4) Audit_group;;
-			5) User_add "$a" "$s";;
+			5) User_add "$2" "$3";;
 			6) Reports_create;;
 			7) Supdate;;
 			8) EPEL;;
 			9) Fail2ban;;
 			10) Report_backup;;
-      11) add_manager;;
-      12) sync;;
+                        11) add_manager;;
+                        12) sync;;
 			[^q]) echo "Wrong choice choose Correct option please ...q to quit...";;
 			q) exit
 		esac
